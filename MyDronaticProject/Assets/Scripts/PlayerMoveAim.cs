0a10379d424df6da6a344800a42d38afd3a68d94
victorelliott <78336692+victorelliott@users.noreply.github.com>
2021-03-11 00:03:20 -0500

    Updated MainGameScene

---
+++ MyDronaticProject/Assets/Scripts/PlayerMoveAim.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveAim : MonoBehaviour
+{
+	public float moveSpeed = 5f;
+	public Rigidbody2D rb;
+	public Camera cam;
+	Vector2 movement;
+	Vector2 mousePos;
+
+	void Awake()
+	{
+		//assign rigidbody2D and camera to variables
+		rb = GetComponent<Rigidbody2D>();
+		cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>() as Camera;
+	}
+
+	void Update()
+	{
+		if (gameObject != null)
+		{
+			//for position, get axis inputs
+			movement.x = Input.GetAxisRaw("Horizontal");
+			movement.y = Input.GetAxisRaw("Vertical");
+			//for rotation: get mouse position 
+			mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		//actual movement uses Rigidbody, so goes in FixedUpdate
+		rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+		//actual rotation uses vector math to calculate angle, and then rotates character towards mouse
+		Vector2 lookDir = mousePos - rb.position;
+		float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+		rb.rotation = angle;
+	}
+}
