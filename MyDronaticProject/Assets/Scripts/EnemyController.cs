953401bd35b20a949b8377c463737b72471c0ed6
sinanunan <sinan.unan@tufts.edu>
2021-03-13 18:34:38 -0500

    Added enemy, coin and timer

---
+++ MyDronaticProject/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private int speed;
+    public GameObject enemy; // TODO
+    public GameObject coinPrefab; // TODO assign coin prefab to this
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void onDeath() {
+
+        GameObject coin = Instantiate(coinPrefab, enemy.transform);
+        Destroy(enemy);
+        // TODO set coin's value to speed 
+    }
+
+}
