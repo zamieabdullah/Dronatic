a15b4cde31f15bce5db356901bdd292bce92987a
zamieabdullah <zamieadbdullah0@gmail.com>
2021-03-15 00:36:41 -0400

    Enemy movements finished

--- MyDronaticProject/Assets/Scripts/EnemyController.cs
+++ MyDronaticProject/Assets/Scripts/EnemyController.cs
@@ -1,30 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class EnemyController : MonoBehaviour
 {
-    private int speed;
-    public GameObject enemy; // TODO
-    public GameObject coinPrefab; // TODO assign coin prefab to this
+    public float speed;
+    public Rigidbody2D rb;
     
-    // Start is called before the first frame update
-    void Start()
+    Vector2 movement;
+    
+    void Awake()
     {
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
+    }
+    
+    void Update()
+    {    
+        movement.x = (float)Math.Cos(transform.rotation.eulerAngles.z);
+        movement.y = (float)Math.Sin(transform.rotation.eulerAngles.z);
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
-
-    void onDeath() {
-
-        GameObject coin = Instantiate(coinPrefab, enemy.transform);
-        Destroy(enemy);
-        // TODO set coin's value to speed 
+    
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        transform.Rotate(new Vector3(0, 0, (transform.rotation.eulerAngles.z + 170) % 360));
+        Debug.Log(transform.rotation.eulerAngles.z);
     }
 
 }
--- MyDronaticProject/Assets/Scripts/PlayerMoveAim.cs
+++ MyDronaticProject/Assets/Scripts/PlayerMoveAim.cs
@@ -4,38 +4,43 @@ using UnityEngine;
 
 public class PlayerMoveAim : MonoBehaviour
 {
-	public float moveSpeed = 5f;
-	public Rigidbody2D rb;
-	public Camera cam;
-	Vector2 movement;
-	Vector2 mousePos;
-
-	void Awake()
-	{
-		//assign rigidbody2D and camera to variables
-		rb = GetComponent<Rigidbody2D>();
-		cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>() as Camera;
-	}
-
-	void Update()
-	{
-		if (gameObject != null)
+		public float moveSpeed = 5f;
+		public Rigidbody2D rb;
+		public Camera cam;
+		Vector2 movement;
+		Vector2 mousePos;
+		
+		void Awake()
 		{
-			//for position, get axis inputs
-			movement.x = Input.GetAxisRaw("Horizontal");
-			movement.y = Input.GetAxisRaw("Vertical");
-			//for rotation: get mouse position 
-			mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+			  //assign rigidbody2D and camera to variables
+			  rb = GetComponent<Rigidbody2D>();
+			  cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>() as Camera;
 		}
-	}
+    
+		void OnCollisionEnter(Collision col)
+		{
+			  Debug.Log(col.collider.name);
+		}
+		
+		void Update()
+		{
+				if (gameObject != null)
+				{
+						//for position, get axis inputs
+						movement.x = Input.GetAxisRaw("Horizontal");
+						movement.y = Input.GetAxisRaw("Vertical");
+						//for rotation: get mouse position 
+						mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+				}
+		}
 
-	void FixedUpdate()
-	{
-		//actual movement uses Rigidbody, so goes in FixedUpdate
-		rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-		//actual rotation uses vector math to calculate angle, and then rotates character towards mouse
-		Vector2 lookDir = mousePos - rb.position;
-		float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-		rb.rotation = angle;
-	}
+		void FixedUpdate()
+		{
+				//actual movement uses Rigidbody, so goes in FixedUpdate
+				rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+				//actual rotation uses vector math to calculate angle, and then rotates character towards mouse
+				Vector2 lookDir = mousePos - rb.position;
+				float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+				rb.rotation = angle;
+		}
 }
